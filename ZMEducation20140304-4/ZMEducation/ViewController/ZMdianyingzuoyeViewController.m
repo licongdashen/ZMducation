bf3443369251afccbc9dfb950b4a948289ba6ccc
licong <queen@QueendeiMac.local>
2016-07-29 10:28:31 +0800

    no message

--- ZMEducation20140304-4/ZMEducation/ViewController/ZMdianyingzuoyeViewController.m
+++ ZMEducation20140304-4/ZMEducation/ViewController/ZMdianyingzuoyeViewController.m
@@ -9,8 +9,8 @@
 #import "ZMdianyingzuoyeViewController.h"
 
 @interface ZMdianyingzuoyeViewController ()
-@property (nonatomic, strong) UIExpandingTextView * TV_Bk1;
-@property (nonatomic, strong) UIExpandingTextView * TV_Bk2;
+@property (nonatomic, strong) UITextView * TV_Bk1;
+@property (nonatomic, strong) UITextView * TV_Bk2;
 @property (nonatomic, strong) UILabel *titleLb;
 @property (nonatomic, strong) UIImageView *contentImagv;
 
@@ -39,25 +39,27 @@
     
     UIImageView *titleImagv = [[UIImageView alloc]initWithFrame:CGRectMake(100,article_Category_View.frame.origin.y + article_Category_View.frame.size.height + 20 , self.view.frame.size.width - 200, 60)];
     titleImagv.image = [UIImage imageNamed:@"Article_Item_11"];
-    titleImagv.userInteractionEnabled = NO;
+    titleImagv.userInteractionEnabled = YES;
     [articleView addSubview:titleImagv];
     
-    self.TV_Bk1 = [[UIExpandingTextView alloc]initWithFrame:CGRectMake(120, 10 , titleImagv.frame.size.width - 120, 40)];
+    self.TV_Bk1 = [[UITextView alloc]initWithFrame:CGRectMake(120, 10 , titleImagv.frame.size.width - 120, 40)];
     self.TV_Bk1.font = [UIFont systemFontOfSize:18];
     self.TV_Bk1.backgroundColor = [UIColor clearColor];
     self.TV_Bk1.text = gousiDict[@"title"];
+    self.TV_Bk1.editable = NO;
     [titleImagv addSubview:self.TV_Bk1];
     
     self.contentImagv = [[UIImageView alloc]initWithFrame:CGRectMake(100,titleImagv.frame.origin.y + titleImagv.frame.size.height + 20 , self.view.frame.size.width - 200, 500)];
     self.contentImagv.image = [UIImage imageNamed:@"Article_Item_12"];
-    self.contentImagv.userInteractionEnabled = NO;
+    self.contentImagv.userInteractionEnabled = YES;
     self.contentImagv.tag = 300;
     [articleView addSubview:self.contentImagv];
     
-    self.TV_Bk2 = [[UIExpandingTextView alloc]initWithFrame:CGRectMake(60, 60 , self.contentImagv.frame.size.width - 120, self.contentImagv.frame.size.height - 120)];
+    self.TV_Bk2 = [[UITextView alloc]initWithFrame:CGRectMake(60, 60 , self.contentImagv.frame.size.width - 120, self.contentImagv.frame.size.height - 120)];
     self.TV_Bk2.font = [UIFont systemFontOfSize:18];
     self.TV_Bk2.backgroundColor = [UIColor clearColor];
     self.TV_Bk2.text = gousiDict[@"articleDraft"];
+    self.TV_Bk2.editable = NO;
     [self.contentImagv addSubview:self.TV_Bk2];
 
 }
